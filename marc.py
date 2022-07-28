7fa2356db744280015bbe3ad442e148720392e81
BriChavez <brianachavez@gmail.com>
2022-07-27 20:37:43 -0700

    moved everything over into a py file, commented, and cleaned

--- marc.py
+++ marc.py
@@ -15,72 +15,77 @@ with open('data/Resume/CHEF/chef_resumes.txt', 'r') as resume_script:
     resume_file = resume_script.read()
     resume_file = resume_file.lower()
 
-
-
-
+# split file string and only keeps words, commas, and periods
 resume_string = re.findall(r"[A-Za-z]+|[.,]", resume_file)
-
-
+# add words to the get rid of pile
 more_drops = ['state', 'city', 'name', 'company', 'college']
+# go through our code and toss out the unwanteds
 stop_dropped_resume = [w for w in resume_string if not w in more_drops]
 
-# # encode the string into ‘ASCII’ and error as ‘ignore’ to remove Unicode characters
-# no_nonsense = str.encode(str(resume_string), "utf-8", "ignore")
-# # # # decode the string back in its form
-# no_nonsense_resume = no_nonsense.decode()
 
-# print(stop_dropped_resume[:1000])
-
-
-# for loop that runs over every word in test
+"""CREATE OUR NESTED DICT TO SAVE HOW OFTEN ONE WORD FOLLOWS ANOTHER"""
 
 resume_dict = {}
-
-for i, word in enumerate(resume_string[:-1]):
-    this_word = resume_string[i+1]
-    # if this_word is in dict, asssign it to 'next_count'
+# for loop that runs over every word in our string, but stating to stop at the last word
+for i, word in enumerate(stop_dropped_resume[:-1]):
+    # setting this word to be the word right after the one we were on
+    this_word = stop_dropped_resume[i+1]
+    # if this_word isnt in our dictionary already...
     if this_word not in resume_dict:
+        # start our counter dict
         next_count = {}
+        # add our new word to be a key in our resume dict and the count dict to be its value
         resume_dict[this_word] = next_count
-
-    # if not, create empty dictionary with this_word as the key and next_count as the value
+    # if it is already in there....
     else:
+        # create empty dictionary with this_word as the key and next_count as the value
         next_count = resume_dict[this_word]
-
+    # if the next word(word) is in our nested dict already...
     if word in next_count:
+        # add one to its count
         next_count[word] += 1
+    # if its not already in there
     else:
+        # lets add it and set its count to 1
         next_count[word] = 1
-this_word = resume_string[i+1]
-
-# print(json.dumps(resume_dict, sort_keys=True, indent=3))
-
-first_word = random.choice(resume_string)
-while first_word in string.punctuation:
-    first_word = random.choice(resume_string)
-print(first_word)
 
 
+"""START THE STORY"""
 
+# starting out story off blank
 story = []
-
-def start_story(str):
-    """function to start the story"""
-    if story == []:
-        # set story up with a first word
-        story.append(first_word)
-    else:
-        # dont start the story if its already been started
-        pass
-    return story
-
-start_story(resume_string)
+# Pick the first word at random from our initial string
+first_word = random.choice(stop_dropped_resume)
+# while loop to make sure we dont start on punctuation
+while first_word in string.punctuation:
+    # if it was punctuation, choose again
+    first_word = random.choice(stop_dropped_resume)
+# if story is blank...
+if story == []:
+    # set story up with our first word
+    story.append(first_word)
 
 
+"""START THE STORY"""
+
+# starting out story off blank
+story = []
+# Pick the first word at random from our initial string
+first_word = random.choice(seuss_string)
+# while loop to make sure we dont start on punctuation
+while first_word in string.punctuation:
+    # if it was punctuation, choose again
+    first_word = random.choice(seuss_string)
+# if story is blank...
+if story == []:
+    # set story up with our first word
+    story.append(first_word)
 
 
+"""CREATE THE WEIGHTED PROB ALGORITHM TO DETERMINE THE NEXT WORD"""
+
+# function to get the next word using weighted probability
 def whos_next():
-    """function to get the next word using weighted probability"""
     # set our current word to be the last word of the story string
     current_word = story[-1]
     # pick out our nested dict for specific word
@@ -93,61 +98,62 @@ def whos_next():
         if outer_word == current_word:
             # add the total times a word followed current word
             total = sum(total_occurances)
-            
-            probs = []
             # set empty variable for the percent chance of each word occuring
-            hopefuls = []
+            probs = []
             # set a list of all words that came after current word
-            likely = []
+            hopefuls = []
             # set an empty list to grab the count of each word
+            likely = []
+            # pull every number of times each word came after current
             for number in total_occurances:
-                # pull every number of times each word came after current
-                percent = number / total
                 # factor the percent chance of a word being next
-                probs.append(percent)
+                percent = number / total
                 # add each percent to our probs list
+                probs.append(percent)
+            # pull words and their values from our inner dict
             for word, num in word_self_total:
-                # pull words and their values from our inner dict
-                hopefuls.append(word)
                 # add each word to our hopefuls list
-                likely.append(num)
+                hopefuls.append(word)
                 # add each number to our likely list
-                global rng_says
-            rng_says = choice(hopefuls, p=probs)
+                likely.append(num)
+            # making our rng_says a global variable
+            global rng_says
             # weighted rng based off the percent probability of word occuring
-            # return rng_says
-            next_word = str(rng_says)
+            rng_says = choice(hopefuls, p=probs)
             # change rng output from numpy to string
-            story.append(next_word)
+            next_word = str(rng_says)
             # add our next word string to the story
+            story.append(next_word)
+            
 
+"""ADD WORDS TO OUR STORY, DROP WHITE SPACE, CAPITALIZE, AND NEWLINE"""
 
-
-
-
-"""add words to the story"""
-
-
-while len(story) < 1500:
+# while our story is less than this long
+while len(story) < 600:
+    # call our function
     whos_next()
+# set our full story to be
 whole_story = ' '.join(story)
-print(whole_story)
-
-
-# this drops the spaces
+# replace white space preceding punctuation
 whole_story = re.sub(r'\s([?.,!](?:\s|$))', r'\1', whole_story)
-
-
+# resplit the story, this time on sentences
 sentences = re.split('[?.]', whole_story)
+
+# set a new clean variable
 cap_sent = []
+# for every sentense in our story...
 for sentence in sentences:
+    # strip the left white space and capitalize the first word and add it to a new list
     cap_sent.append((sentence.lstrip().capitalize() + '.'))
+# turn our story back into a string by joining them back in together
 cap_sent = ' '.join(cap_sent)
 
-
-random.randint(5, 10)
+# back to splitting our text on periods
 splittext = cap_sent.split(".")
+# for every 5 to 10 sentences...
 for x in range(5, len(splittext), random.randint(5, 10)):
+    # add a new line and start the next sentence tabbed in
     splittext[x] = "\n"+"\t"+splittext[x].lstrip()
+# throw it all back together into a string
 text = ".".join(splittext)
-print(text)
+
