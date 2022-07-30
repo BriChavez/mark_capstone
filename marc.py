0fef80ad790086a1ceb8007cf795b31332084505
BriChavez <brianachavez@gmail.com>
2022-07-30 15:03:02 -0700

    add idiot proof comments to decribel how to use the code depending on your use

--- marc.py
+++ marc.py
@@ -2,17 +2,44 @@ import language_tool_python
 import re
 import string
 import random
+import os
 from numpy.random import choice
+import contractions
 
 
-"""LET'S GET STARTED"""
+# ------------------------------OPTION ONE---------------------------------
+""" ------------USE THIS SECTION FOR THE VARIABLE APPROACH--------------"""
 
-# open our file
-with open('data/bri/data_stack_resumes.txt', 'r') as resume_script:
+# ----ENTER SUBJECT OF RESUME YOU WANT TO USE----- replace the underline, leave the quotes
+parriable = "________"
+# sets the folder path to a variable
+folder_path = f'data/Resume/{parriable.upper()}'
+# saves the folder name for later(i know this is redundant, but im keeping it cause its pretty)
+resume_folder = os.path.basename(folder_path).lower()
+# now, we open the txt of the subject you have chosen
+with open(f'{folder_path}/{resume_folder}_resumes.txt', 'r') as resume_script:
+    # open txt file and read to string
+    resume_file = resume_script.read().lower()
+# if you go with this option, highlight all of option 2 and press ctrl and the ?/ button to comment it out
+# follow the instructions at the bottom of this page to change the default save
+
+
+# ------------------------------OPTION TWO---------------------------------
+"""--------USE THIS SECTION FOR USING A FILE PATH OF YOUR CHOOSING-------"""
+
+# -----PASTE YOUR FILEPATH IN THE EMPTY SPOT---- replace the underline, leave the quotes
+with open('__________', 'r') as resume_script:
     # open txt file and read to string
     resume_file = resume_script.read()
     # lowercase everything in the string
     resume_file = resume_file.lower()
+# by default, your generated resume will save as resume.txt in the finished_resumes folder
+# follow the instructions at the bottom of the page to change this
+
+# ---------------------------------------------------------------------------
+
+
+"""NOW, LET'S GET STARTED"""
 
 # regex to keep only punctuation or words more than two letters long, may or may not contain an apostrophe in the middle
 resume_string = re.findall(r"[.,]|[a-z]+[']?[a-z]+", resume_file)
@@ -21,15 +48,21 @@ more_drops = ['state', 'city', 'name', 'company',
               'college', 'am', 'the', 'to', 'in']
 # toss those words to the curbs
 stop_dropped_resume = [w for w in resume_string if not w in more_drops]
+# Here, we will expand contractions
+expanded_words = []
+for word in stop_dropped_resume:
+  # using contractions.fix to expand the shortened words
+  expanded_words.append(contractions.fix(word))
+
 
 
 """CREATE OUR NESTED DICT TO SAVE HOW OFTEN ONE WORD FOLLOWS ANOTHER"""
 
 resume_dict = {}
 # for loop that runs over every word in our string, but stating to stop at the last word
-for i, word in enumerate(stop_dropped_resume[:-1]):
+for i, word in enumerate(expanded_words[:-1]):
     # setting this word to be the word right after the one we were on
-    this_word = stop_dropped_resume[i+1]
+    this_word = expanded_words[i+1]
     # if this_word isn't in our dictionary already...
     if this_word not in resume_dict:
         # start our counter dict
@@ -50,6 +83,7 @@ for i, word in enumerate(stop_dropped_resume[:-1]):
         next_count[word] = 1
 
 
+
 """START THE STORY"""
 
 # starting out story off blank
@@ -64,6 +98,7 @@ while first_word in string.punctuation:
 if story == []:
     # set story up with our first word
     story.append(first_word)
+
 
 
 """CREATE THE WEIGHTED PROB ALGORITHM TO DETERMINE THE NEXT WORD"""
@@ -110,9 +145,11 @@ def whos_next():
             story.append(next_word)
             
 
+
 """ADD WORDS TO OUR STORY, DROP WHITE SPACE, CAPITALIZE, AND NEWLINE"""
 
 # -----ADD WORDS-------
+
 # while our story is less than this long
 while len(story) < 400:
     # call our function
@@ -122,7 +159,9 @@ whole_story = ' '.join(story)
 # apparently, the code like to run forever unless we put a print function here
 print(whole_story)
 
-# ------REMOVE WHITESPACE AND CAPITALIZE FIRST WORD OF SENTENCES------
+
+# ------REMOVE WHITESPACE BEFORE PERIODS AND CAPITALIZE FIRST WORD OF SENTENCES------
+
 # replace white space preceding punctuation
 whole_story = re.sub(r'\s([?.,!](?:\s|$))', r'\1', whole_story)
 # re split the story, this time on sentences
@@ -136,11 +175,13 @@ for sentence in sentences:
 # turn our story back into a string by joining them back in together
 cap_sent = ' '.join(cap_sent)
 
+
 # -------ADD NEWLINES AND BEGINNING OF PARAGRAPH TABS------
+
 # back to splitting our text on periods
 splittext = cap_sent.split(".")
 # for every 5 to 10 sentences...
-for x in range(4, len(splittext), random.randint(5, 8)):
+for x in range(4, len(splittext), random.randint(4, 8)):
     # add a new line and start the next sentence tabbed in
     splittext[x] = "\n"+"\t"+splittext[x].lstrip()
 # throw it all back together into a string
@@ -148,17 +189,23 @@ text = ".".join(splittext)
 # add a tab to the first line so it matches its friends
 text = f'\t'+text
 
-# -------SPELL CHECK AND SAVE------
+
+# -------SPELL CHECK AND SAVE-------
+
 # set our spell check function to english and assign it to a variable
 tool = language_tool_python.LanguageTool('en-US')
 # set our checker to auto correct
 checked_text = tool.correct(text)
 # open a new file in the generated resume folder
-text_file = open(f'finished_resumes/generated_resumes.txt', 'w')
+"""TO CHANGE DEFAULT SAVE"""
+# ----OPTION 1----uncomment out the line directly below this. To do this, highlight the text and press ctrl and the ?/ button
+# text_file = open(f'finished_resumes/{resume_folder}_resume.txt', 'w')
+# ----IF YOU WANT TO SPECIFY FILE NAME OR FOLDER PATH----
+# enter a new file path or resume name in the quotes after the letter f below
+text_file = open(f'finished_resumes/generated_resume.txt', 'w')
 # write our resume to it
-text_file.write(text)
+text_file.write(checked_text)
 # shut it down. our work here is done
 text_file.close()
-
 
 """THE END"""
