d544cf969d6e0b8c344aa2a19b7ddc0ed51bc449
BriChavez <brianachavez@gmail.com>
2022-08-01 02:51:05 -0700

    I hate when it makes me do this

--- pdf_converter.py
+++ pdf_converter.py
@@ -4,17 +4,19 @@ import os
 
 
 # SET RESUME THEME YOU WOULD LIKE TO ACCESS
-variable = 'BPO'
+variable = 'TEACHER'
 
 # compile all the PDF filenames in that folder:
-folder_path = f'data/Resume/{variable}'
-# extract folder name based off the variable 
+folder_path = f'data/Resume/{variable.upper()}'
+# extract folder name based off the variable
 # ---(i know this is redundant, but I wrote it before the variable and i thought it to be clever, so its still in here)---
 resume_folder = os.path.basename(folder_path).lower()
 
 
 """function to take all resume pdfs from an variable type folder and read them all into one big giant folder named for is folder type"""
 # let us begin the extraction process
+
+
 def extract_resumes():
     # set an empty variable to store our file names in as we read them
     pdfFiles = []
@@ -45,15 +47,19 @@ def extract_resumes():
     # our work here is done.
     pdfOutput.close()
 
+
 # runs the function to turn all pdfs in a stated folder into one giant pdf
 extract_resumes()
 
 
 """function to change our newly made giant pdf into a txt file so we can play with it"""
 # let us begin the transformation process
+
+
 def big_pdf2txt():
     # open up our new big ol pdf using the same folder variable as before
-    all_resumes_pdf = open(f'{folder_path}/{resume_folder}_resumes.pdf', mode='rb')
+    all_resumes_pdf = open(
+        f'{folder_path}/{resume_folder}_resumes.pdf', mode='rb')
     # use our reader to read our doc
     all_resumes_file = PyPDF2.PdfFileReader(all_resumes_pdf)
     # set an empty string to catch our page text
@@ -64,12 +70,13 @@ def big_pdf2txt():
         page = all_resumes_file.getPage(pageNum)
         # extract text from the page
         text += page.extract_text() + "\n"
-    # open a new txt file in our directory as a write   
+    # open a new txt file in our directory as a write
     text_file = open(f'{folder_path}/{resume_folder}_resumes.txt', "w")
     # write the text we collected from our pdf pages to the file
     text_file.write(text)
     # our work here is done
     text_file.close()
+
 
 # call the function to transform a big pdf file into a txt file
 big_pdf2txt()
